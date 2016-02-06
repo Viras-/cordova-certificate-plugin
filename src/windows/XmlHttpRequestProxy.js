b032bc9f896e2a2f3e739f97fcfb9796050babda
Viras- <viras@users.sourceforge.net>
2016-02-06 20:01:10 +0100

    delegate implementations for xmlhttprequest

--- src/windows/XmlHttpRequestProxy.js
+++ src/windows/XmlHttpRequestProxy.js
@@ -54,41 +54,139 @@ XmlHttpRequestProxy.prototype = {
     // functions
     abort: function () {
         console.log('XmlHttpRequestProxy - abort');
-    },
-    getAllResponseHeaders: function () {
-        console.log('XmlHttpRequestProxy - getAllResponseHeaders');
-    },
-    getResponseHeader: function () {
-        console.log('XmlHttpRequestProxy - getResponseHeader');
-    },
-    open: function (bstrMethod, bstrUrl, varAsync, bstrUser, bstrPassword) {
-        console.log('XmlHttpRequestProxy - open');
 
-        this.httpClient = new Windows.Web.Http.HttpClient();
-        this.method = bstrMethod;
-        this.uri = new Windows.Foundation.Uri(window.location.href, bstrUrl);
-        this.async = varAsync;
-    },
-    send: function () {
-        console.log('XmlHttpRequestProxy - send');
-
-        if (this.method === 'GET') {
-            var me = this;
-            this.httpClient.getStringAsync(this.uri).done(function (response) {
-                console.log('XmlHttpRequestProxy - send - done');
-            });
+        if (this.isLocal) {
+            this.xmlHttpRequest.abort.apply(this.xmlHttpRequest, arguments);
         } else {
 
         }
     },
+    getAllResponseHeaders: function () {
+        console.log('XmlHttpRequestProxy - getAllResponseHeaders');
+
+        if (this.isLocal) {
+            this.xmlHttpRequest.getAllResponseHeaders.apply(this.xmlHttpRequest, arguments);
+        } else {
+
+        }
+    },
+    getResponseHeader: function () {
+        console.log('XmlHttpRequestProxy - getResponseHeader');
+
+        if (this.isLocal) {
+            this.xmlHttpRequest.getResponseHeader.apply(this.xmlHttpRequest, arguments);
+        } else {
+
+        }
+    },
+    open: function (bstrMethod, bstrUrl, varAsync, bstrUser, bstrPassword) {
+        console.log('XmlHttpRequestProxy - open');
+
+        // check if request is local
+        if (bstrUrl.indexOf("http") !== 0) {
+            this.isLocal = true;
+
+            // create normal XMLHttpRequest for local requests
+            this.xmlHttpRequest = new this.delegate();
+            this.xmlHttpRequest.onload = this.bind(this, function () {
+                // mirror status etc.
+                this.status = this.xmlHttpRequest.status;
+                this.statusText = this.xmlHttpRequest.statusText;
+                this.responseText = this.xmlHttpRequest.responseText;
+
+                if (this.onload) {
+                    this.onload.apply(this, arguments);
+                }
+            });
+
+            // forward various callbacks
+            this.forwardCallback("onloadstart");
+            this.forwardCallback("onprogress");
+            this.forwardCallback("onabort");
+            this.forwardCallback("onerror");
+            this.forwardCallback("ontimeout");
+            this.forwardCallback("onloadend");
+            this.forwardCallback("onreadystatechange");
+
+            // forward open call
+            this.xmlHttpRequest.open(bstrMethod, bstrUrl, varAsync, bstrUser, bstrPassword);
+
+        } else {
+            // create httpclient for remote requests
+            this.isLocal = false;
+            this.httpClient = new Windows.Web.Http.HttpClient();
+
+            // remember request parameters
+            this.method = bstrMethod;
+            this.uri = new Windows.Foundation.Uri(window.location.href, bstrUrl);
+            this.async = varAsync;
+        }
+    },
+    send: function (data) {
+        console.log('XmlHttpRequestProxy - send');
+
+        if (this.isLocal) {
+            this.xmlHttpRequest.send.apply(this.xmlHttpRequest, arguments);
+        } else {
+
+            if (this.method === 'GET') {
+                this.httpClient.getStringAsync(this.uri).done(this.bind(this, function (response) {
+                    console.log('XmlHttpRequestProxy - send - get - done');
+                    this.status = 200;
+                    this.statusText = "";
+                    this.response = response;
+
+                    if (this.onload) {
+                        this.onload();
+                    }
+                }));
+            } else {
+                this.httpClient.postAsync(this.uri, data).done(this.bind(this, function (response) {
+                    console.log('XmlHttpRequestProxy - send - post - done');
+                    this.status = 200;
+                    this.statusText = "";
+                    this.response = response;
+
+                    if (this.onload) {
+                        this.onload();
+                    }
+                }));
+            }
+        }
+    },
     setRequestHeader: function () {
         console.log('XmlHttpRequestProxy - setRequestHeader');
+
+        if (this.isLocal) {
+            this.xmlHttpRequest.setRequestHeader.apply(this.xmlHttpRequest, arguments);
+        } else {
+
+        }
     },
     // internal variables
     httpClient: null,
+    xmlHttpRequest: null,
     method: 'GET',
     uri: null,
-    async: true
+    async: true,
+    isLocal: false,
+    // helper functions
+    bind: function (context, func) {
+        return function () {
+            func.apply(context, arguments);
+        };
+    },
+    /**
+     * Fowards a given callback from the internal XMLHttpRequest to our proxy
+     * @param String name
+     */
+    forwardCallback: function (name) {
+        this.xmlHttpRequest[name] = this.bind(this, function () {
+            if (this[name]) {
+                this[name].apply(this, arguments);
+            }
+        });
+    }
 };
 
 module.exports = XmlHttpRequestProxy;
